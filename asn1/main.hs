a55c71bdd8ce1f6650c02de48100f8287b3af618
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2020-01-29 12:46:07 -0600

    Update main.hs

--- asn1/main.hs
+++ asn1/main.hs
@@ -14,7 +14,13 @@ tens n = fromIntegral(product (take n [10, 10 .. ]))
 rd :: Int -> [Double] -> [Double]
 rd n x = map (\r -> fromIntegral(truncate(r * fromIntegral(tens n))) / fromIntegral(tens n)) x
 
+-- Creates a Double array given (from, to, count)
+absolute :: [(Double, Double)] -> [Double]
+absolute [] = []
+absolute ((x,y) : xs) = (sqrt (x^2 + y^2)) : (absolute xs)
+
 -- Main driver
 main = do
   print (range 0 1 10)
   print (rd 2 [2.123,3.456,4.675])
+  print (absolute [(1,2), (3,4)] )
